e3f2fbb167292a26a22aa89359964414fada2f88
Ben Schenkenberger and Riley Watts <ben@>
2017-03-16 15:17:50 -0700

    add .set() functionality to fill answer array

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -1,3 +1,4 @@
+import java.io.Console;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -7,9 +8,33 @@ public class Hangman {
   private String mAnswer;
   private String mGuesses;
 
-  String[] possibleAnswers = {"cheese"};
+  // String[] possibleAnswers = {"cheese"};
+
 
   public String getAnswer(){
-    return mAnswer = possibleAnswers[0];
+    return mAnswer = "trophy";
+  }
+
+  public String checkLetter(String letter){
+    String gameSolution = "trophy";
+    List<String> solutionArray = new ArrayList<String>();
+    for(int a = 0; a<gameSolution.length(); a++){
+      solutionArray.add("_");
+    }
+    System.out.println(solutionArray.size());
+    int letterIndex = gameSolution.indexOf(letter);
+    System.out.println(letterIndex);
+    if(letterIndex !=-1){
+      solutionArray.set(letterIndex, letter);
+      System.out.println(solutionArray.get(letterIndex));
+      return "ok";
+    }else{
+      return "error";
+    }
   }
 }
+
+
+// for(int i = 0; i < solutionLetters.length; i++ ){
+//   letter.indexOf()
+// }
--- src/test/java/HangmanTest.java
+++ src/test/java/HangmanTest.java
@@ -12,9 +12,21 @@ public class HangmanTest {
   @Test
   public void newHangman_receivesAnswer_String() {
     Hangman testHangman = new Hangman();
-    assertEquals("cheese", testHangman.getAnswer());
+    assertEquals("trophy", testHangman.getAnswer());
   }
 
+  // @Test
+  // public void newHangman_checkLetterAgainstAnswer_String() {
+  //   Hangman testHangman = new Hangman();
+  //   assertEquals("h", testHangman.checkLetter("h"));
+  // }
+
+  @Test
+  public void newHangman_correctLetterAddedToAnswerArray_Array() {
+    Hangman testHangman = new Hangman();
+
+    assertEquals("p", testHangman.checkLetter("p"));
+  }
 }
 
 
@@ -22,7 +34,17 @@ public class HangmanTest {
 /*
 1. Instantiating the constructor correctly
 2. Receives answer string from a created/stored array.
+Recognize an individual letter within the answer string.
+
+- Start a hangman game (Instantiate a new constructor object)
+- The game will select a word from a stored list of "answers".
+- The game will recognize a letter from within that word.
+
+If you pass "A" through the test, it will check that letter
+against all letters in the stored answer.
+
+
 
 
 Create a secondary array with underscore characters that will correspond to the length of the
-answer that has been selected. 
+answer that has been selected.*/
